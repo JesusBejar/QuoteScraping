ffbe44258229d40a767f09b5ad9bea0dc194f2af
Jesus Bejar <israelbejarii@gmail.com>
2024-12-13 07:41:05 -0700

    added request and parser

--- program2.py
+++ program2.py
@@ -9,7 +9,14 @@ from random import choice
 all_books = []
 
 # constant part of url
-base_url = 'http://books.toscrape.com'
+base_url = 'http://books.toscrape.com/'
 # changing part of url
 url = '/page/1'
 
+while url:
+
+    res = requests.get(f"{base_url}{url}")
+    print(f'I am now parsing {base_url}{url}! beep beep boop boop')
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    # class name found in html page
