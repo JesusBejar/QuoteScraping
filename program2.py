ba8f421adb1f04e2e4545159cbd10153763d93f2
Jesus Bejar <israelbejarii@gmail.com>
2024-12-16 11:37:16 -0700

    added program2 urls

--- program2.py
+++ program2.py
@@ -9,31 +9,8 @@ from random import choice
 all_books = []
 
 # constant part of url
-base_url = 'http://books.toscrape.com/'
+base_url = 'https://books.toscrape.com/'
 # changing part of url
-url = '/page/1'
+url = 'catalogue/page-1.html'
 
-while url:
 
-    res = requests.get(f"{base_url}{url}")
-    print(f'I am now parsing {base_url}{url}! beep beep boop boop')
-    soup = BeautifulSoup(res.text, 'html.parser')
-
-    # class name found in html page
-    books_on_page = soup.find_all(class_='product_pod')
-
-    for book in books_on_page:
-        all_books.append({
-			'title': book.find(title_='').get_text(),
-            'stock': book.find(class_='instock availability').get_text(),
-			'price': book.find(class_='price_color').get_text()
-        })
-    next_btn = soup.find(_class='next')
-    url = next_btn.find("a")["href"] if next_btn else None
-    sleep(2)
-
-    book = choice(all_books)
-    print("Here is some book info: ")
-    print(book["title"])
-    print(book["stock"])
-    print(book["price_color"])
